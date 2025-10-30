530afd3f51c8a54135cfe1f00dd82b4cc146b43d
Savitskaya Margarita <savitskayari@yandex.ru>
2025-10-30 21:21:36 +0300

    Task04 & Task05

--- src/Controller.php
+++ src/Controller.php
@@ -1,11 +1,49 @@
 <?php
 
+
 namespace SavickayaM\TicTacToe;
 
 use SavickayaM\TicTacToe\View;
+use SavickayaM\TicTacToe\Model;
+
+use function cli\line;
 
 class Controller
 {
+    public function menu()
+    {
+        $isExist = file_exists('tic-tac-toe.db');
+
+        View::showGame($isExist);
+
+        $choiceUser = readline("Выберите действие: ");
+
+        switch ($choiceUser) {
+            case 1:
+                $this->startGame();
+                break;
+            case 2:
+                if ($isExist) {
+                    $this->showHistory();
+                    break;
+                } else {
+                    echo "Таблицы не существует";
+                    break;
+                }
+            case 3:
+                if ($isExist) {
+                    $this->showGameRepeat();
+                    break;
+                } else {
+                    echo "Таблицы не существует";
+                    break;
+                }
+            default:
+                echo "Неверный выбор";
+                break;
+        }
+    }
+
     private function validationData(string $playerData): array|bool
     {
         $inputArray = explode(' ', $playerData);
@@ -33,12 +71,13 @@ class Controller
     private function isСrosses(): bool
     {
         $isCrosses = rand(0, 1);
-        return $isCrosses === 1;
+
+        return $isCrosses === 1 ? true : false;
     }
 
     private function isEmpty(string $cell): bool
     {
-        return $cell === ' ';
+        return $cell === ' ' ? true : false;
     }
 
     private function isWinner(array $board, string $symbol): bool
@@ -69,15 +108,31 @@ class Controller
             $y = rand(0, $n - 1);
         } while (!$this->isEmpty($board[$x][$y]));
 
-        return [$x, $y];
+        $ceil = [$x, $y];
+
+        return $ceil;
     }
 
     public function startGame(): void
     {
         $board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+        $ceil = [];
+
         $computerMove = '';
         $playerMove = '';
+        $numberTry = 0;
+
         $moves = 0;
+
+        $playerName = readline("Введите ваше имя: ");
+
+        if (! file_exists('tic-tac-toe.db')) {
+            $model = new Model('tic-tac-toe.db');
+            $model->createTables();
+        } else {
+            $model = new Model('tic-tac-toe.db');
+        }
+        $gameId = $model->createId();
 
         try {
             if ($this->isСrosses()) {
@@ -103,11 +158,14 @@ class Controller
 
                 if (!$this->validationData($playerData)) {
                     View::showErrorMessage();
-                    return;
+                    die();
                 }
 
                 $ceil = explode(' ', $playerData);
+
                 $board[$ceil[0] - 1][$ceil[1] - 1] = $playerMove;
+                $moves++;
+                $numberTry++;
 
                 View::showBoard($board);
 
@@ -116,46 +174,79 @@ class Controller
 
                 echo "Ход компьютера:\n";
                 View::showBoard($board);
+                $moves++;
+                $computerCeilView = $computerCeil[0] + 1 . ' ' . $computerCeil[1] + 1;
 
-                $moves += 2;
+                $model->storeTry($gameId, $numberTry, $playerData, $computerCeilView);
             }
+
+            $computerCeilView = $computerCeil[0] + 1 . ' ' . $computerCeil[1] + 1;
 
             while (true) {
                 $playerData = readline("Введите координаты ячейки (в формате x y, например 1 1): ");
 
                 if (!$this->validationData($playerData)) {
                     View::showErrorMessage();
-                    continue;
+                    die();
                 }
 
                 $ceil = explode(' ', $playerData);
 
                 if ($this->isEmpty($board[$ceil[0] - 1][$ceil[1] - 1])) {
                     $board[$ceil[0] - 1][$ceil[1] - 1] = $playerMove;
+                    $numberTry++;
+                    $moves++;
 
                     $computerCeil = $this->generateComputerMove($board);
                     $board[$computerCeil[0]][$computerCeil[1]] = $computerMove;
+                    $moves++;
 
                     View::showBoard($board);
-                    $moves += 2;
+                    $computerCeilView = $computerCeil[0] + 1 . ' ' . $computerCeil[1] + 1;
+
+                    $model->storeTry($gameId, $numberTry, $playerData, $computerCeilView);
                 } else {
-                    View::showHint();
+                    View::showHint($board);
                     continue;
                 }
 
                 if ($this->isWinner($board, $playerMove)) {
                     View::showWin();
+                    $model->storeResult($playerName, $playerMove, '3x3', $playerMove);
                     break;
                 } elseif ($this->isWinner($board, $computerMove)) {
                     View::showLose();
+                    $model->storeResult($playerName, $playerMove, '3x3', $computerMove);
                     break;
-                } elseif ($moves >= 9) {
+                } elseif ($moves === count($board) * count($board)) {
+                    $model->storeResult($playerName, $playerMove, '3x3', '-');
                     View::showDraw();
                     break;
                 }
             }
         } catch (\Exception $e) {
             View::showErrorMessage();
+            die();
         }
+    }
+
+    public function showHistory(): void
+    {
+        $model = new Model('tic-tac-toe.db');
+
+        $games = $model->getGames();
+
+        View::showHistory($games);
+    }
+
+    public function showGameRepeat()
+    {
+        $gameId = readline("Введите номер сохраненной партии: ");
+
+        $model = new Model('tic-tac-toe.db');
+
+        $game = $model->getGame($gameId);
+
+        View::showGameRepeat($game);
     }
 }
